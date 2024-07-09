de5f620d37c7652245d63a4cda697af1483b1375
adomasr <adomas.razmus@gmail.com>
2024-07-09 02:13:26 +0100

    first steps

---
+++ date/app.js
@@ -0,0 +1,18 @@
+const findYear = (month, dayOfWeek)=> {
+    let year = 0;
+
+    for(let i=2014; i<=2050; i++) {
+        const current = new Date(i, month, 1);
+
+        if(current.getDay() === dayOfWeek) {
+            year = i;
+            break;
+        };
+    };
+    return year;
+};
+
+
+
+
+
